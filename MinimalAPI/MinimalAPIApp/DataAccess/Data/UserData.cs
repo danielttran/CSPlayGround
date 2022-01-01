b3a355f56f061cc47b5da69aa4d475a55e3c7961
Daniel <tutran.cs@gmail.com>
2021-12-31 22:06:19 -0500

    Initial

---
+++ MinimalAPI/MinimalAPIApp/DataAccess/Data/UserData.cs
@@ -0,0 +1,44 @@
+﻿using DataAccess.DbAccess;
+using DataAccess.Models;
+
+namespace DataAccess.Data;
+
+public class UserData : IUserData
+{
+    private readonly ISqlDataAccess _db;
+    public UserData(ISqlDataAccess db)
+    {
+        _db = db;
+    }
+
+    public Task<IEnumerable<UserModel>> GetUsers()
+    {
+        return _db.LoadData<UserModel, dynamic>("dbo.spUser_GetAll",
+                                                new { });
+    }
+
+    public async Task<UserModel?> GetUser(int id)
+    {
+        var result = await _db.LoadData<UserModel, dynamic>("dbo.spUser_Get",
+                                                            new { Id = id });
+        return result.FirstOrDefault();
+    }
+
+    public Task InsertUser(UserModel user)
+    {
+        return _db.SaveData("dbo.spUser_Insert",
+                            new { user.FirstName, user.LastName });
+    }
+
+    public Task UpdateUser(UserModel user)
+    {
+        return _db.SaveData("dbo.spUser_Update",
+                            new { user });
+    }
+
+    public Task DeleteUser(int id)
+    {
+        return _db.SaveData("dbo.spUser_Delete", new { Id = id });
+    }
+
+}
---
+++ MinimalAPI/MinimalAPIApp/UserDB/Script.PostDeployment.sql
@@ -0,0 +1,10 @@
+﻿IF NOT EXISTS (SELECT 1 FROM dbo.[User])
+BEGIN
+	-- fast way to check if there is any row
+	INSERT INTO dbo.[User] (FirstName, LastName)
+	VALUES 
+		('John', 'Smith'),
+		('David', 'Storm'),
+		('Mary', 'Jones'),
+		('Tim', 'Corey');
+END
